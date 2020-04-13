c70c669be429ffbfab630b9531c738dc1ee92144
Samigirma1 <samuel_appsandsubscriptions@yahoo.com>
2020-04-13 06:00:09 -0400

    homework1 created

---
+++ lab09/lab09_3.py
@@ -0,0 +1,53 @@
+from keras.datasets import imdb
+from keras import models, layers, optimizers
+
+import numpy as np
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+def vectorize_sequences(sequences, dimension=10000):
+    # Create an all-zero matrix of shape (len(sequences), dimension)
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.  # set specific indices of results[i] to 1s
+    return results
+
+# Our vectorized training data
+x_train = vectorize_sequences(train_data)
+# Our vectorized test data
+x_test = vectorize_sequences(test_data)
+# Our vectorized labels
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+# create a validation set
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+# suggested model
+# suggestedModel = models.Sequential()
+# suggestedModel.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+# suggestedModel.add(layers.Dense(16, activation='relu'))
+# suggestedModel.add(layers.Dense(1, activation='sigmoid'))
+# suggestedModel.compile(optimzer = optimizers.RMSprop(lr = 0.001), loss = "binary_crossentropy", metrics = ["accuracy"])
+#
+# history = suggestedModel.fit(partial_x_train,
+#                     partial_y_train,
+#                     epochs=20,
+#                     batch_size=512,
+#                     validation_data=(x_val, y_val))
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=4, batch_size=512)
+results = model.evaluate(x_test, y_test)
