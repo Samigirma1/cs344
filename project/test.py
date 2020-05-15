82504afe3096947e7a7493d4bbef82b07eeb8227
Samigirma1 <samuel_appsandsubscriptions@yahoo.com>
2020-05-15 05:52:09 -0400

    model trained on limited data - more training data

--- project/test.py
+++ project/test.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import cv2 as cv
 from PIL import Image
 import argparse
+
 def detectAndDisplay(frame):
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_gray = cv.equalizeHist(frame_gray)
@@ -16,24 +17,10 @@ def detectAndDisplay(frame):
         faceROI = frame_gray[y:y+h,x:x+w]
         cv.imwrite("result" + str(i) + ".jpeg", faceROI)
         i+=1
-        #-- In each face, detect eyes
-        # eyes = eyes_cascade.detectMultiScale(faceROI)
-        # for (x2,y2,w2,h2) in eyes:
-        #     eye_center = (x + x2 + w2//2, y + y2 + h2//2)
-        #     radius = int(round((w2 + h2)*0.25))
-        #     frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
-    # cv.imwrite('result.jpeg', frame)
-    # cv.imwrite("result2.jpeg",faceROI)
-    #cv.imshow('Capture - Face detection', frame)
-# parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-# parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-# parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-# parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
-# args = parser.parse_args()
+
 face_cascade_name = "./openCVcascades/face_detector.xml"#args.face_cascade
-#eyes_cascade_name = args.eyes_cascade
 face_cascade = cv.CascadeClassifier()
-# eyes_cascade = cv.CascadeClassifier()
+
 #-- 1. Load the cascades
 if not face_cascade.load(face_cascade_name):
     print('--(!)Error loading face cascade')
@@ -41,20 +28,3 @@ if not face_cascade.load(face_cascade_name):
 
 im = cv.imread("./hard.jpg")
 detectAndDisplay(im)
-# if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
-#     print('--(!)Error loading eyes cascade')
-#     exit(0)
-#camera_device = args.camera
-#-- 2. Read the video stream
-#cap = cv.VideoCapture(camera_device)
-# if not cap.isOpened:
-#     print('--(!)Error opening video capture')
-#     exit(0)
-# while True:
-#     ret, frame = cap.read()
-#     if frame is None:
-#         print('--(!) No captured frame -- Break!')
-#         break
-#     detectAndDisplay(frame)
-#     if cv.waitKey(10) == 27:
-#         break
